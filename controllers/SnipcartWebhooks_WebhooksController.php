ea4a5d596c10f2395429df860cbeaf61279c08c6
Charles Ouellet <charles.ouellet@gmail.com>
2014-04-10 10:20:25 -0400

    Allowed anonymous users, starting to handle the POST request

--- controllers/SnipcartWebhooks_WebhooksController.php
+++ controllers/SnipcartWebhooks_WebhooksController.php
@@ -4,4 +4,31 @@ namespace Craft;
 
 class SnipcartWebhooks_WebhooksController extends BaseController {
 	
+    protected $allowAnonymous = true;
+
+	public function actionPost() {
+		$this->requirePostRequest();
+		$json = file_get_contents('php://input');
+		$body = json_decode($json, true);
+
+		$this->returnJson($json);
+		if (is_null($body) or !isset($body['eventName'])) {
+		    // When something goes wrong, return an invalid status code
+		    // such as 400 BadRequest.
+		    header('HTTP/1.1 400 Bad Request');
+		    return;
+		}
+
+		switch ($body['eventName']) {
+		    case 'order.completed':
+		        // This is an order:completed event
+		        // do what needs to be done here.
+		    	$this->processOrderCompletedEvent($body);
+		    	break;
+		}
+	}
+
+	private function processOrderCompletedEvent($data) {
+		$this->returnJson($data);
+	}
 }
